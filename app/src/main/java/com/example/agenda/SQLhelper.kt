dff2e6897938d13d51706b5be351ed040c3a5fd8
Alumno <noemilopezpina@gmail.com>
2022-07-27 17:15:45 +0200

    commit Design

--- app/src/main/java/com/example/agenda/SQLhelper.kt
+++ app/src/main/java/com/example/agenda/SQLhelper.kt
@@ -7,15 +7,24 @@ import android.database.sqlite.SQLiteOpenHelper
 import com.example.agenda.ui.list.ListFragment
 import com.example.agenda.ui.newcontact.NewFragment
 
-class SQLhelper(context: Context?): SQLiteOpenHelper(context, "AddressBook.db", null, 1) {
+class SQLhelper(context: Context?): SQLiteOpenHelper(context, DATABASE, null, 1) {
+
+    companion object {
+        const val DATABASE = "AddessBook"
+        const val TABLE_NAME ="contacts"
+        const val FIELD_ID="_id"
+        const val FIELD_NAME="name"
+        const val FIELD_SURNAME="surname"
+        const val FIELD_MAIL="mail"
+        const val FIELD_PHONE="phonee"
+    }
     override fun onCreate(db: SQLiteDatabase?) {
-        val commandCreate = "CREATE TABLE contacts"+
-                "(_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "name TEXT," +
-                "surname TEXT,"+
-                "email TEXT,"+
-                "phone TEXT,"+
-                "picture NUMBER)"
+        val commandCreate = "CREATE TABLE $TABLE_NAME"+
+                "($FIELD_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "$FIELD_NAME TEXT," +
+                "$FIELD_SURNAME TEXT,"+
+                "$FIELD_MAIL TEXT,"+
+                "$FIELD_PHONE TEXT)"
         db!!.execSQL(commandCreate)
 
     }
@@ -27,10 +36,10 @@ class SQLhelper(context: Context?): SQLiteOpenHelper(context, "AddressBook.db", 
     fun addData (name: String, surname:String, email: String, phone:String ) {
         // ContentValues tiene una estructura de tipo Map()
         val data = ContentValues()
-        data.put("name", name)
-        data.put("surname", surname)
-        data.put("email", email)
-        data.put("phone", phone)
+        data.put(FIELD_NAME, name)
+        data.put(FIELD_SURNAME, surname)
+        data.put(FIELD_MAIL, email)
+        data.put(FIELD_PHONE, phone)
         // Abro la DB en modo ESCRITURA
         val db = this.writableDatabase
         db.insert("contacts", null,data)
